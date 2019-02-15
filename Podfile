85410c45e3118b630661ca6a60ccf700bbbac70b
Smirnov Maxim <atimca@gmail.com>
2019-02-15 12:22:10 +0300

    album service

--- Podfile
+++ Podfile
@@ -2,5 +2,5 @@ platform :ios, '12.1'
 
 target 'PhotoGallery' do
   use_frameworks!
-  pod 'AlamofireImage', '3.5.0'
+  pod 'Kingfisher', '5.1.1'
 end
---
+++ Sources/Code/Core/Models/Album.swift
@@ -0,0 +1,11 @@
+//
+// Copyright © 2019 Smirnov Maxim. All rights reserved. 
+//
+
+import Foundation
+
+struct Album: Codable {
+    let id: Int
+    let title: String
+    let userId: Int
+}
